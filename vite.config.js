a51679824cda98f742310f0c01e256548068830a
ehabahmed2 <ihapppc@gmail.com>
2025-12-05 15:41:24 +0200

    root url

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // important for GitHub Pages
+  base: '/react-jobs', // important for GitHub Pages
   server: {
     port: 3000,
     proxy: {
